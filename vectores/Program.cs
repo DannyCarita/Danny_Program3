f75d4ed177036bc7f1f4b88855ea9204dbbd5a29
DannyCarita <dannycjimenez00@gmail.com>
2024-03-12 19:04:20 -0400

    vector

--- vectores/Program.cs
+++ vectores/Program.cs
@@ -1,56 +1,20 @@
-﻿using System;
+﻿using Systen;
 
-static class Program
+static class program{
+
+static void Main(string[] args)
 {
-        static void Main(string[] args)
-        {
-        /*    
-        //vector de numeros
-        int[] numeros = new int[] {1, 2, 3, 4, 5, };
+    int[] vector = {2, 4, 6, 8, 10, 9, 15};
+    sumVector(vector);
+    multiplotres(vector);
 
-        //vector de letras
-        int[] letras = new char[] {'a', 'b', 'c', 'd', 'e' };
-
-        //vecotres de palabras
-        int[] palabras = new string[] {"hola", "mundo", "es", "genial" };
-
-        //generar un vector vacio
-        int[] vacio = new int[10];
-        Console.WriteLine("\nvector vacio:");
-        imprimirVector(vacio);
-        */
-        int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        cambiodedatos();
-
-        int temp = cambio [3];
-        cambio[3] = cambio [2];
-        cambio[2] = temp;
-        Console.WriteLine("\n Vector con posicion intercambiada; ");
-        imprimirVector(cambio);
-
-        }
-        
-        static void imprimirVector(int[] vector)
-        {
-            string vectorString = "";
-            foreach(int elemento in vector) 
-            {
-                vectorString = vectorString + elemento + " ";
-            }
-            Console.WriteLine(vectorString);
-
-        }
-        static void cambiodedatos()
-        {
-        int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        imprimirVector(numeros);
-
-        numeros[2] = 100;
-        imprimirVector(numeros);
-        //tareas viernes
-        }
-        
-
-} 
+static void sumVector(int[] vector)
+{
+    int sum = 0;
+ 
+}
+static int[] ingresar[] vector, int 
+}
 
 
+}
