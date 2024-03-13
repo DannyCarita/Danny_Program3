78be6563f43491c978b4f85c5f96b7a308b5fec6
DannyCarita <dannycjimenez00@gmail.com>
2024-03-12 21:13:06 -0400

    TODOS LOS VECTORES

--- vectores/Program.cs
+++ vectores/Program.cs
@@ -1,66 +1,74 @@
 ﻿using System;
-
 static class Program{
-
-static void Main(string[] args)
-{
-        LlenarVector(10);
-}
-static void LlenarVector(int cantidad)
-{
-    int[] vector = new int[cantidad];
-    int numero = 1;
-
-    for (int i = 0; i < cantidad; i++)
+    static void Main(string[] args)
     {
-        while (!esPrimo(numero))
-        {
+        //llenarVector(6);
+        Console.WriteLine("dame una palabra: ");
+        string palabra = Console.ReadLine();
+        llenarVectorstring(palabra);
+    }
+    static void llenarVectorimpar(int cantidad){
+        int[] vector = new int[cantidad];
+        int numero= 1;
+        for(int i=0; i< cantidad; i++){
+            while(!esimpar(numero)){
+                numero++;
+            }
+            vector[i]=numero;
             numero++;
         }
-        vector[i] = numero;
-        numero++;
+        imprimirVector(vector);
     }
-    imprimirVector(vector);
-/*FUNCION PARA OBTENER IMPARES    
-}
-static bool esPar(int numero){
-    if ((numero % 2)==1){
+    static void llenarVectorPrimo(int cantidad){
+        int[] vector = new int[cantidad];
+        int numero= 1;
+        for(int i=0; i< cantidad; i++){
+            while(!esPrimo(numero)){
+                numero++;
+            }
+            vector[i]=numero;
+            numero++;
+        }
+        imprimirVector(vector);
+    }
+    static bool esPrimo(int numero){
+        if(numero <= 1){
+            return false;
+        }
+        for(int i = 2; i * i <= numero; i++){
+            if(numero % i == 0 ){
+                return false;
+            }
+        }
         return true;
     }
-    else{
-        return false;
-    }
-
-
-}
-*/
-static bool esPrimo(int numero)
-{
-    if(numero <= 1)
-    {
-        return false;
-    }
-    for(int i = 2; i + i <= numero; i++)
-    {
-        if(numero % 1 == 0)
-        {
+    static bool esimpar(int numero){
+        if((numero % 2)==1){
+            return true;
+        }
+        else{
             return false;
         }
     }
-    return true;
-}
-
-
-
-
-static void imprimirVector(int[] vector)
-{
-    foreach(int elemento in vector)
-    {
-        Console.Write(elemento + " ");
+    static void imprimirVector(int[] vector){
+        foreach(int elemento in vector){
+            Console.Write(elemento + " ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-}
-
-}
+    //vector letra char
+    static void llenarVectorstring(string palabra){
+        
+        char[] letras = new char[palabra.Length];
+        for( int i= 0; i < palabra.Length;i++){
+            letras[i] = palabra[i];
+        }
+        imprimirVectorletras(letras);
+    }
+    static void imprimirVectorletras(char[] letras){
+        foreach(char letra in letras){
+            Console.Write(letra + " ");
+        }
+        Console.WriteLine();
+    }
 }
